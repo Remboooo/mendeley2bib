976252751d2f1246bae8221955975fe5fb68d28a
Rembrand van Lakwijk <rem@lakwijk.com>
2012-09-13 17:00:43 +0200

    Added options to select Mendeley document folder (collection), and option to only process starred items

--- mendeley2bib.py
+++ mendeley2bib.py
@@ -56,8 +56,44 @@ class Mendeley2Bib:
         def __exit__(self, type, value, traceback):
             self.conn.close()
 
-        def fetchEntries(self):
-            return self.conn.execute('SELECT * FROM Documents WHERE deletionPending = \'false\';').fetchall()
+        def getEntries(self, folder=None, onlyFavourites=False):
+            query = 'SELECT * FROM Documents AS d WHERE deletionPending != \'true\''
+            params = []
+            if folder is not None:
+                if folder == 0:
+                    query = '%s AND d.id NOT IN (SELECT documentID FROM DocumentFolders)' % query
+                else:
+                    query = '%s AND d.id IN (SELECT documentID FROM DocumentFolders AS df WHERE df.folderId = ?)' % query
+                    params.append(folder)
+            if onlyFavourites:
+                query = '%s AND d.favourite = \'true\'' % query
+            query = '%s;' % query
+            print(query)
+            return self.conn.execute(query, params).fetchall()
+            
+        def getFolders(self):
+            folders = {}
+            rows = self.conn.execute('SELECT * FROM Folders;').fetchall()
+            for row in rows:
+                folders[row['id']] = row
+            names = {0: '/'}
+            def getFolderName(index, rows):
+                row = rows[index]
+                parent = getFolderName(row['parentId'], rows) if row['parentId'] > 0 else ''
+                return '%s/%s' % (parent, row['name'])
+            for (id, folder) in folders.items():
+                names[id] = getFolderName(id, folders)
+            return names
+        
+        def getFolderID(self, identifier):
+            if identifier.isdigit():
+                matches = lambda id, item: int(id) == int(item[0])
+            else:
+                matches = lambda id, item: id == item[1]
+            for folder in self.getFolders().items():
+                if matches(identifier, folder):
+                    return folder[0]
+            return None
 
         def getDocumentContributors(self, entry, type, concat=True):
             authors = self.conn.execute('SELECT * FROM DocumentContributors WHERE contribution=? AND documentId=?', [type, entry['id']]).fetchall()
@@ -114,7 +150,10 @@ if __name__=='__main__':
 
     argparser = ArgumentParser(description='Convert Mendeley entries to a Biblatex-compatible bib file')
     argparser.add_argument('-d', '--dbfile', metavar='NAME', help='The database to load. Use -l to list all available databases. Required when more than one database is available.', default=defaultDB)
+    argparser.add_argument('-f', '--folder', metavar='FOLDER', help='The folder to process entries from. By default all folders are traversed. Use --lf to see available folders. May be either given as ID or name; when the argument is numeric, it is assumed to be the ID.', default=None)
+    argparser.add_argument('-s', '--starred', dest='onlyFavourites', action='store_const', const=True, default=False, help='Only process starred (favourite) items')
     argparser.add_argument('-l', '--list', dest='list', action='store_const', const=True, default=False, help='In stead of processing a database, list available databases.')
+    argparser.add_argument('-lf', '--list-folders', dest='listfolders', action='store_const', const=True, default=False, help='Just list all available Mendeley folders')
     argparser.add_argument('-k', '--write-keys', dest='writeback_keys', action='store_const', const=True, default=False, help='When an absent citation key is generated, write it back to the Mendeley database. NOTE: this only works when Mendeley Desktop is not running, since it locks its database')
     argparser.add_argument('-v', '--verbose', dest='loglevel', action='store_const', const=logging.DEBUG, default=logging.INFO, help='Set debug level to DEBUG in stead of INFO')
     args = argparser.parse_args()
@@ -130,7 +169,7 @@ if __name__=='__main__':
         if choices is 0:
             print('None! Please connect to a Mendeley account first using Mendeley Desktop')
         sys.exit(0)
-
+        
     if not args.dbfile:
         print('Please specify the database file to use. Choices are:')
         choices = 0
@@ -141,12 +180,25 @@ if __name__=='__main__':
             print('None! Please connect to a Mendeley account first using Mendeley Desktop')
         sys.exit(-1)
 
+    if args.listfolders:
+        with m2b.openDatabase(args.dbfile) as db:
+            print('Available Mendeley folders:')
+            for (id, folder) in sorted(db.getFolders().items(), key=lambda x: x[1]):
+                print('%d: %s' % (id, folder))
+            sys.exit(0)
+        
     latex.register()
 
     numConverted = 0
 
     with m2b.openDatabase(args.dbfile) as db:
-        for entry in db.fetchEntries():
+        folderID = None
+        if args.folder:
+            folderID = db.getFolderID(args.folder)
+            if folderID is None:
+                log.error('Folder \'%s\' not found! Use --lf to list available folders.' % args.folder)
+                sys.exit(-1)
+        for entry in db.getEntries(folder=folderID, onlyFavourites=args.onlyFavourites):
             converted = db.convertEntry(entry, bibtemplates)
             if converted:
                 print(dedent(converted))
